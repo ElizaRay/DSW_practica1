ac9867672775c2b954846f3627e9e5dc8f964511
Elizabeth Rayon Ramirez <82187811+ElizaRay@users.noreply.github.com>
2021-08-30 17:04:36 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';//Librerias
+
+function miApp(){ //Función principal
+  return( //Retorno
+    //Contenido
+    <div>
+    <h1>Hola a todos</h1> 
+    <p>Este es el primer ejemplo y funciona perfecto</p>
+    </div>
+  );
+}
+
+export default miApp; //export Resultado
